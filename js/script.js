4a0d9e0a6870810700c7409c66955b2efe5ac098
Deoprag <pdroesofiarabelo@gmail.com>
2022-03-24 14:19:59 -0300

    Visual changes
    Added box-shadow and changed output style

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
             var n1plus = Number(tn1plus.value);
             var n2plus = Number(tn2plus.value);
             var splus = n1plus + n2plus ;
-            res1.innerHTML = `${n1plus} plus ${n2plus} is equal to <strong>|${splus}|<strong>`;
+            res1.innerHTML = `${n1plus} plus ${n2plus} is equal to: <strong>[${splus}]<strong>`;
         } // This block calcs addition
 
         function less(){
@@ -16,7 +16,7 @@
             var n1less = Number(tn1less.value);
             var n2less = Number(tn2less.value);
             var sless = n1less - n2less ;
-            res2.innerHTML = `${n1less} minus ${n2less} is equal to <strong>|${sless}|<strong>`;
+            res2.innerHTML = `${n1less} minus ${n2less} is equal to: <strong>[${sless}]<strong>`;
         } // This block calcs substraction
 
         function multiply(){
@@ -26,7 +26,7 @@
             var n1multiply = Number(tn1multiply.value);
             var n2multiply = Number(tn2multiply.value);
             var smultiply = n1multiply * n2multiply;
-            res3.innerHTML = `${n1multiply} times ${n2multiply} is equal to <strong>|${smultiply}|<strong>`;
+            res3.innerHTML = `${n1multiply} times ${n2multiply} is equal to: <strong>[${smultiply}]<strong>`;
         } // This block calcs multiplication
 
         function divide(){
@@ -39,6 +39,6 @@
             if (n1divide == 0 || n2divide == 0){ // This if was applied because any value divided by 0 is 0.
                 res4.innerHTML = `You cannot divide by 0. Try again`;
             } else {
-            res4.innerHTML = `${n1divide} divided by ${n2divide} is equal to <strong>|${sdivide}|<strong>`;
+            res4.innerHTML = `${n1divide} divided by ${n2divide} is equal to: <strong>[${sdivide}]<strong>`;
             } // This block calcs division
         }
