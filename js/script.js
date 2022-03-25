7d3ebbc1248721e75d54f2ac7d44eaefc97ca757
Deoprag <pdroesofiarabelo@gmail.com>
2022-03-25 14:03:14 -0300

    Update js
    Changed getElementById to querySelector

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
         function plus(){
-            var tn1plus = window.document.getElementById('txtn1-plus');
+            var tn1plus = window.document.querySelector('input#txtn1-plus');
             var tn2plus = window.document.querySelector('input#txtn2-plus');
-            var res1 = window.document.getElementById ('res1');
+            var res1 = window.document.querySelector ('div#res1');
             var n1plus = Number(tn1plus.value);
             var n2plus = Number(tn2plus.value);
             var splus = n1plus + n2plus ;
@@ -10,9 +10,9 @@
         } // This block calculates addition
 
         function less(){
-            var tn1less = window.document.getElementById('txtn1-less');
+            var tn1less = window.document.querySelector('input#txtn1-less');
             var tn2less = window.document.querySelector('input#txtn2-less');
-            var res2 = window.document.getElementById ('res2');
+            var res2 = window.document.querySelector ('div#res2');
             var n1less = Number(tn1less.value);
             var n2less = Number(tn2less.value);
             var sless = n1less - n2less ;
@@ -20,9 +20,9 @@
         } // This block calculates substraction
 
         function multiply(){
-            var tn1multiply = window.document.getElementById('txtn1-multiply');
+            var tn1multiply = window.document.querySelector('input#txtn1-multiply');
             var tn2multiply = window.document.querySelector('input#txtn2-multiply');
-            var res3 = window.document.getElementById ('res3');
+            var res3 = window.document.querySelector ('div#res3');
             var n1multiply = Number(tn1multiply.value);
             var n2multiply = Number(tn2multiply.value);
             var smultiply = n1multiply * n2multiply;
@@ -30,9 +30,9 @@
         } // This block calculates multiplication
 
         function divide(){
-            var tn1divide = window.document.getElementById('txtn1-divide');
+            var tn1divide = window.document.querySelector('input#txtn1-divide');
             var tn2divide = window.document.querySelector('input#txtn2-divide');
-            var res4 = window.document.getElementById ('res4');
+            var res4 = window.document.querySelector ('div#res4');
             var n1divide = Number(tn1divide.value);
             var n2divide = Number(tn2divide.value);
             var sdivide = n1divide / n2divide;
